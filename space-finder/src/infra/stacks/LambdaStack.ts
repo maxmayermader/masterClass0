4581f7ac151c9b98930883b201f14d74afa53efc
Max Mayer-Mader <max.mayer-mader@covestro.com>
2023-06-21 21:06:34 +0200

    6/21 first

--- space-finder/src/infra/stacks/LambdaStack.ts
+++ space-finder/src/infra/stacks/LambdaStack.ts
@@ -1,32 +1,34 @@
 import { Stack, StackProps } from 'aws-cdk-lib'
 import { LambdaIntegration } from 'aws-cdk-lib/aws-apigateway';
 import { ITable } from 'aws-cdk-lib/aws-dynamodb';
-import { Construct } from 'constructs';
-import { join } from 'path';
+import { Effect, PolicyStatement } from 'aws-cdk-lib/aws-iam';
 import { Runtime} from 'aws-cdk-lib/aws-lambda';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
-
+import { Construct } from 'constructs';
+import { join } from 'path';
 interface LambdaStackProps extends StackProps {
     spacesTable: ITable
 }
 
 export class LambdaStack extends Stack {
 
-    public readonly helloLambdaIntegration: LambdaIntegration
+    public readonly spacesLambdaIntegration: LambdaIntegration
 
     constructor(scope: Construct, id: string, props: LambdaStackProps) {
         super(scope, id, props)
 
 
-        const helloLambda = new NodejsFunction(this, 'HelloLambda', {
+        const spacesLambda = new NodejsFunction(this, 'SpacesLambda', {
             runtime: Runtime.NODEJS_18_X,
             handler: 'handler',
-            entry: (join(__dirname, '..','..', 'services', 'hello.ts')),
+            entry: (join(__dirname, '..','..', 'services', 'spaces', 'handler.ts')),
             environment: {
                 TABLE_NAME: props.spacesTable.tableName
             }
-        })
+        });
 
-        this.helloLambdaIntegration = new LambdaIntegration(helloLambda)
+        this.spacesLambdaIntegration = new LambdaIntegration(spacesLambda)
+        
+
     }
 }
