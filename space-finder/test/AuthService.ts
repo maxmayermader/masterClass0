512bd4f5e633dc6418befa38cf70eda37d865f70
Max Mayer-Mader <max.mayer-mader@covestro.com>
2023-07-12 10:04:41 +0200

    final

--- space-finder/test/AuthService.ts
+++ space-finder/test/AuthService.ts
@@ -1,5 +1,7 @@
 import { CognitoUser } from '@aws-amplify/auth';
 import { Amplify, Auth } from 'aws-amplify';
+import { CognitoIdentityClient } from '@aws-sdk/client-cognito-identity';
+import { fromCognitoIdentityPool } from '@aws-sdk/credential-providers'
 
 const awsRegion = 'eu-central-1'
 
@@ -8,7 +10,8 @@ Amplify.configure({
         region: awsRegion,
         userPoolId: 'eu-central-1_tUrrRW9oJ',
         userPoolWebClientId: '7o9ki3oujlsdl3a1mq6anapj3u',
-        authenticationFlowType: 'USER_PASSWORD_AUTH'
+        identityPoolId: 'eu-central-1:a9accd0f-a63c-4d32-8e2b-7a6331ee4ce2',
+        authenticationFlowType: 'USER_PASSWORD_AUTH'       
     }
 });
 
@@ -20,5 +23,20 @@ export class AuthService {
     public async login(userName: string, password: string) {
         const result = await Auth.signIn(userName, password) as CognitoUser;
         return result;
+    }
+
+    public async generateTemporaryCredentials(user: CognitoUser){
+        const jwtToken = user.getSignInUserSession().getIdToken().getJwtToken();
+        const cognitoIdentityPool = `cognito-idp.${awsRegion}.amazonaws.com/eu-central-1_tUrrRW9oJ`;
+        const cognitoIdentity = new CognitoIdentityClient({
+            credentials: fromCognitoIdentityPool({
+                identityPoolId: 'eu-central-1:a9accd0f-a63c-4d32-8e2b-7a6331ee4ce2',
+                logins: {
+                    [cognitoIdentityPool]: jwtToken
+                }
+            })
+        });
+        const credentials = await cognitoIdentity.config.credentials();
+        return credentials;
     }
 }
--- space-finder/test/auth.test.ts
+++ space-finder/test/auth.test.ts
@@ -1,3 +1,4 @@
+import { ListBucketsCommand, S3Client } from "@aws-sdk/client-s3";
 import { AuthService } from "./AuthService";
 
 
@@ -7,6 +8,20 @@ async function testAuth(){
         'max1',
         'Max12345$'
     )
-    console.log(loginResult.getSignInUserSession().getIdToken().getJwtToken());}
+    // console.log(loginResult.getSignInUserSession().getIdToken().getJwtToken());
+    const credentials = await service.generateTemporaryCredentials(loginResult);
+    //console.log(credentials);
+    const buckets = await listBuckets(credentials);
+    console.log(buckets);
+}
+
+async function listBuckets(credentials: any){
+    const client = new S3Client({
+        credentials: credentials
+    });
+    const command = new ListBucketsCommand({});
+    const result = await client.send(command);
+    return result;
+}
 
 testAuth();
